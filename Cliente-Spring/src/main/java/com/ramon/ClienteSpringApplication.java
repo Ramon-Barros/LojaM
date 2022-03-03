0fe8d75976f1f9841527007eda8d3aafa9de0bea
Ramon Barros <ramonfilipe@gmail.com>
2022-03-03 20:45:23 -0300

    lado do spring criado para o projeto

---
+++ Cliente-Spring/src/main/java/com/ramon/ClienteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ramon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteSpringApplication.class, args);
+	}
+
+}
