a184065348a4e85e1133020df1fb7d79ca113a52
Ramon Barros <ramonfilipe@gmail.com>
2022-03-04 11:35:11 -0300

    testando um olá mundo

--- Cliente-Spring/src/main/java/com/ramon/ClienteSpringApplication.java
+++ Cliente-Spring/src/main/java/com/ramon/ClienteSpringApplication.java
@@ -2,10 +2,17 @@ package com.ramon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClienteSpringApplication {
 
+	@GetMapping("/ola")
+	public String helloWorld(){
+		return "Olá Mundo!!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteSpringApplication.class, args);
 	}
