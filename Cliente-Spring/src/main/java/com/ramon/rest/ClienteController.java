bd746333916bde018b9d2e614285acf8a5abfc2f
Ramon Barros <ramonfilipe@gmail.com>
2022-03-15 22:34:21 -0300

    continuando com  a criação da api clientes

--- Cliente-Spring/src/main/java/com/ramon/rest/ClienteController.java
+++ Cliente-Spring/src/main/java/com/ramon/rest/ClienteController.java
@@ -1,8 +1,31 @@
 package com.ramon.rest;
 
+import com.ramon.model.enity.Cliente;
+import com.ramon.model.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
+
+    private final ClienteRepository repository;
+
+    @Autowired
+    public ClienteController(ClienteRepository repository){
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente  salvar(Cliente cliente ){
+        return repository.save(cliente);
+
+    }
     
 }
